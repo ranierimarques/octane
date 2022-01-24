14bb45bb9e4e2abc1b838fd517ea9a47075800be
Raniere Marques <Raniere852@outlook.com>
2022-01-24 07:36:13 -0300

    modals

--- src/layout/index/why-choose-octane/modal/modal.jsx
+++ src/layout/index/why-choose-octane/modal/modal.jsx
@@ -2,11 +2,18 @@ import { useState } from 'react'
 
 import * as S from './modal.styles'
 
-function Modal({ infos: icon, title, showMore }) {
+function Modal({ infos: { icon, title, showMore } }) {
   const [modalOpen, setModalOpen] = useState(false)
 
-  const handleOpenModal = () => setModalOpen(true)
-  const handleCloseModal = () => setModalOpen(false)
+  const handleOpenModal = () => {
+    document.body.style.overflowY = 'hidden'
+    setModalOpen(true)
+  }
+
+  const handleCloseModal = () => {
+    document.body.style.overflowY = 'overlay'
+    setModalOpen(false)
+  }
 
   return (
     <>
@@ -15,7 +22,18 @@ function Modal({ infos: icon, title, showMore }) {
       <S.Wrapper isOpen={modalOpen}>
         <S.Overlay onClick={handleCloseModal} />
 
-        <S.Modal></S.Modal>
+        <S.Modal>
+          <S.Header>
+            <S.Icon>{icon}</S.Icon>
+            <S.Title>{title}</S.Title>
+          </S.Header>
+
+          <S.Description>{showMore}</S.Description>
+
+          <S.Button onClick={handleCloseModal}>Ok</S.Button>
+
+          <S.CloseMenu onClick={handleCloseModal} />
+        </S.Modal>
       </S.Wrapper>
     </>
   )
