49a793b5fe6be49d329b3c25178ec4f1b23ec1f3
Raniere Marques <Raniere852@outlook.com>
2022-06-05 15:22:46 -0300

    feat(_app): add favicon

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,8 @@
+import { Footer, Navbar } from '@/common'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 import Head from 'next/head'
 import { useEffect } from 'react'
-import AOS from 'aos'
-
-import { Navbar, Footer } from '@/common'
-
-import 'aos/dist/aos.css'
 import '../styles/global-styles.css'
 
 function MyApp({ Component, pageProps }) {
@@ -27,7 +25,29 @@ function MyApp({ Component, pageProps }) {
           type="font/woff2"
           crossOrigin="anonymous"
         />
-        <link rel="icon" href="/favicon.ico" sizes="32x32" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#121212" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#121212" />
+        <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+        <meta name="theme-color" content="#121212" />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width" />
       </Head>
