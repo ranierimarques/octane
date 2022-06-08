23994f209bb40ec13b3a36d5244534f1d166751f
Raniere Marques <Raniere852@outlook.com>
2022-06-08 14:56:58 -0300

    feat(pages/index): add meta tags of SEO in home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,28 @@
-import Head from 'next/head'
-
-import { Home, WhyChooseOctane, HowWeWork, Faq } from '@/layout/index'
+import { Faq, Home, HowWeWork, WhyChooseOctane } from '@/layout/index'
 import { LastCall } from '@/shared'
+import Head from 'next/head'
 
 function Index() {
   return (
     <main>
       <Head>
-        <title>Octane | A inovação começa aqui!</title>
+        <title>Octane - Construção de sites modernos.</title>
+        <meta
+          name="description"
+          content="Somos um estúdio de design e desenvolvimento de software. Realizamos entregas nacionais e internacionais, somos referência em design e código de qualidade."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://octanestd.com" />
+        <meta property="og:title" content="Octane - Construção de sites modernos." />
+        <meta
+          property="og:description"
+          content="Somos um estúdio de design e desenvolvimento de software. Realizamos entregas nacionais e internacionais, somos referência em design e código de qualidade."
+        />
+        <meta
+          property="og:image"
+          content="https://octanestd.com/sharing-cards/home.jpg"
+        />
+        <meta property="twitter:card" content="summary_large_image" />
       </Head>
 
       <Home />
